2ede06619aca3471b6b5ff9b38cd6450d556970d
wen kai <clwk94.work@gmail.com>
2021-08-07 20:41:26 +0800

    fix: update environment variable

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import pyAesCrypt
 # get credential file
 cwd = os.getcwd()
 password = os.environ['fast_api_password']
-salt = os.environ['fast_api_salt'].encode('utf-8')
 encrypted_file_path = "{}/credentials/credentials.json.aes".format(cwd)
 decrypted_file_path = "{}/credentials/credentials.json".format(cwd)
 pyAesCrypt.decryptFile(encrypted_file_path,
