676e76cfcfeab1824319b3a905d676a875a24e48
kaiwork <86189303+kaiwork@users.noreply.github.com>
2021-08-07 21:32:13 +0800

    Fast api server (#1)
    * feat: create api to get current user data from firebase
    
    * fix: fix CI Pipeline issues

---
+++ sphinx-docs/source/tests.rst
@@ -0,0 +1,29 @@
+tests package
+=============
+
+Submodules
+----------
+
+tests.conftest module
+---------------------
+
+.. automodule:: tests.conftest
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+tests.test\_get\_user module
+----------------------------
+
+.. automodule:: tests.test_get_user
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: tests
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ tests/test_get_user.py
@@ -0,0 +1,49 @@
+from main import app
+from main import get_current_user
+from fastapi.testclient import TestClient
+
+
+class AuthBypasser:
+    """AuthBypasser Class for testing."""
+
+    def __init__(self):
+        """Testing data to show is authenticated."""
+        self.user_id = "is authenticated"
+
+
+def auth_bypass():
+    """
+    Mock auth bypasser to be passed into the authenticated test.
+
+    Returns:
+        AuthBypasser: A simple class to send back the required attribute
+    """
+    return AuthBypasser()
+
+
+client = TestClient(app)
+
+
+def test_get_current_user_not_authenticated():
+    """
+    Test unauthenticated request to get user data.
+
+    Returns:
+        an unauthenticated response
+    """
+    response = client.get("/user/")
+    assert response.status_code == 401
+    assert response.json() == {'detail': 'Not authenticated'}
+
+
+def test_get_current_user_authenticated():
+    """
+    Test authenticated request to get user data.
+
+    Returns:
+        a authenticated response which is a mock of the user data sent back
+    """
+    app.dependency_overrides[get_current_user] = auth_bypass
+    response = client.get("/user/")
+    assert response.status_code == 200
+    assert response.json() == {'id': 'is authenticated'}
